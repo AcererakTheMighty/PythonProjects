1bb74ae12595be9330175c5e161391772b462def
Landon Smith <creeperthemighty@gmail.com>
2024-07-02 15:40:17 -0600

    Started the Password Generator Project

---
+++ passwordgenerator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    
+
+generate_password(10)
