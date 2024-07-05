511c5ce0960d80b037da9dc0c6d5dff4f236f755
Landon Smith <creeperthemighty@gmail.com>
2024-07-05 17:06:24 -0600

    Continued the Checkers AI Project

--- Checkers/checkersinner/piece.py
+++ Checkers/checkersinner/piece.py
@@ -1,8 +1,8 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from checkersinner.constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 import pygame
 
 class Piece:
-    PADDING = 10
+    PADDING = 15
     OUTLINE = 2
 
     def __init__(self, row, col, color):
@@ -31,6 +31,13 @@ class Piece:
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius + self.OUTLINE)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height//2))
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
 
     def __repr__(self):
         return str(self.color)
